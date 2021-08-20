67108e2b81ca311e54a2e48ec5bd0b4d6866d6ef
caverym <caverym@disroot.org>
2021-08-20 13:48:06 -0400

    basename example
    Signed-off-by: caverym <caverym@disroot.org>

---
+++ examples/basename.rs
@@ -0,0 +1,63 @@
+use jargon::Jargon;
+
+struct Args {
+    multiple: bool,
+    suffix: Option<String>,
+    zero: bool,
+    names: Vec<String>,
+}
+
+fn main() {
+    let mut j: Jargon = Jargon::from_env();
+
+    let args = Args {
+        multiple: j.contains(["-a", "--multiple"]),
+        suffix: j.option_arg(["-s", "--suffix"]),
+        zero: j.contains("--zero"),
+        names: j.finish(),
+    };
+
+    if !args.multiple {
+        print(&args, &args.names[0])
+    } else {
+        args.names.iter().for_each(|name| print(&args, name));
+    }
+
+    if !args.zero {
+        println!()
+    }
+}
+
+fn print(args: &Args, name: &String) {
+    if let Some(name) = name.split('/').last() {
+        if let Some(suffix) = &args.suffix {
+            if name.ends_with(suffix) {
+                if let Some(n) = name.strip_suffix(suffix) {
+                    if args.zero {
+                        print!("{}", n)
+                    } else {
+                        print!("{} ", n)
+                    }
+                } else {
+                    if args.zero {
+                        print!("{}", name)
+                    } else {
+                        print!("{} ", name)
+                    }
+                }
+            } else {
+                if args.zero {
+                    print!("{}", name)
+                } else {
+                    print!("{} ", name)
+                }
+            }
+        } else {
+            if args.zero {
+                print!("{}", name)
+            } else {
+                print!("{} ", name)
+            }
+        }
+    }
+}
